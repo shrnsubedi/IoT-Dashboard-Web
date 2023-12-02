46ec9c9a3c355c76c706aff4e2399e71833a76c3
Shrijan Subedi <shrnsubedi22@gmail.com>
2023-12-02 16:24:00 +0100

    - [x] update html and css

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 from typing import Any
 
 from django.http import JsonResponse
@@ -17,6 +18,9 @@ class DashboardDataView(generic.TemplateView):
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context["room_names"] = [room.name for room in Room.objects.all()]
+        context["today_date"] = datetime.now().date
+        context["today_temperature"] = "10C"
+
         return context
 
 
